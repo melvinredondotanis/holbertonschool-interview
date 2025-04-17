949f5d93ffc72a8ac7a03a1d992c56c5b876b9f8
Melvin Redondo--Tanis <melvin@redondotanis.com>
2025-04-17 13:55:38 +0200

    Refactor variable names in canUnlockAll function for consistency

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -5,14 +5,14 @@
 def canUnlockAll(boxes):
     """Checks if all boxes can be unlocked"""
     n = len(boxes)
-    unlocked = [False] * n
-    unlocked[0] = True
+    unclk = [False] * n
+    unclk[0] = True
     keys = boxes[0].copy()
 
     while keys:
         key = keys.pop()
-        if 0 <= key < n and not unlocked[key]:
-            unlocked[key] = True
-            keys.extend([k for k in boxes[key] if 0 <= k < n and not unlocked[k]])
+        if 0 <= key < n and not unclk[key]:
+            unclk[key] = True
+            keys.extend([k for k in boxes[key] if 0 <= k < n and not unclk[k]])
 
-    return all(unlocked)
+    return all(unclk)
