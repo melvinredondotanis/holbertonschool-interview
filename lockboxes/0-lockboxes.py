b1822d791e1423f038bd655f46a1f50deec790e5
Melvin Redondo--Tanis <melvin@redondotanis.com>
2025-04-17 13:54:25 +0200

    Remove unnecessary blank lines in canUnlockAll function for improved readability

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -8,11 +8,11 @@ def canUnlockAll(boxes):
     unlocked = [False] * n
     unlocked[0] = True
     keys = boxes[0].copy()
-    
+
     while keys:
         key = keys.pop()
         if 0 <= key < n and not unlocked[key]:
             unlocked[key] = True
             keys.extend([k for k in boxes[key] if 0 <= k < n and not unlocked[k]])
-    
+
     return all(unlocked)
