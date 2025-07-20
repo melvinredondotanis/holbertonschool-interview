ad9f17a85974268b13107d83bf1f3873e37f6a3a
Melvin Redondo--Tanis <melvin@redondotanis.com>
2025-07-20 22:12:57 +0200

    refactor: replace request library with native https module for fetching starwars film and character data

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,24 +1,54 @@
 #!/usr/bin/node
 
-const request = require('request');
-const filmUrl = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
-request(filmUrl, (error, response, body) => {
-  if (error) {
-    return;
-  }
-  const film = JSON.parse(body);
-  const urlToName = {};
-  film.characters.forEach(characterUrl => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        return;
-      }
-      const character = JSON.parse(body);
-      urlToName[character.url] = character.name;
-      if (Object.keys(urlToName).length === film.characters.length) {
-        film.characters.forEach(characterUrl =>
-          console.log(urlToName[characterUrl]));
-      }
-    });
+const https = require('https');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+https.get(apiUrl, (response) => {
+  let data = '';
+
+  response.on('data', (chunk) => {
+    data += chunk;
   });
+
+  response.on('end', () => {
+    if (response.statusCode !== 200) {
+      console.error(`Error: Status code ${response.statusCode}`);
+      return;
+    }
+
+    const filmData = JSON.parse(data);
+    const charactersUrls = filmData.characters;
+
+    const fetchCharacterNames = (urls, index = 0) => {
+      if (index >= urls.length) return;
+
+      https.get(urls[index], (charResponse) => {
+        let charData = '';
+
+        charResponse.on('data', (chunk) => {
+          charData += chunk;
+        });
+
+        charResponse.on('end', () => {
+          if (charResponse.statusCode !== 200) {
+            console.error(`Error: Status code ${charResponse.statusCode}`);
+            return;
+          }
+
+          const characterData = JSON.parse(charData);
+          console.log(characterData.name);
+
+          fetchCharacterNames(urls, index + 1);
+        });
+      }).on('error', (charError) => {
+        console.error(charError);
+      });
+    };
+
+    fetchCharacterNames(charactersUrls);
+  });
+}).on('error', (error) => {
+  console.error(error);
 });
